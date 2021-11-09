7dad03150c9ef247ae675e3ea8081f6ccfdf3c1c
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2021-11-09 20:02:24 +0100

    I think part 2 finished

--- p2/scripts/master_install.sh
+++ p2/scripts/master_install.sh
@@ -7,7 +7,7 @@ cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 400 ~/.ssh/authorized_keys
 chown root:root ~/.ssh/authorized_keys
 
-curl --insecure https://releases.rancher.com/install-docker/20.10.sh | sh
+sh /tmp/19.03.sh
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --tls-san $(hostname) --bind-address=${current_ip} --advertise-address=${current_ip} --node-ip=${current_ip}" sh -s - --docker
 
@@ -17,7 +17,6 @@ chmod +r /etc/rancher/k3s/k3s.yaml
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 
 ##PODS##
-cat /tmp/app1.yaml >> ~/.app1.yaml
+cat /tmp/apps_config.yaml >> ~/.apps_config.yaml
 
-sleep 30
-/usr/local/bin/kubectl create -f ~/.app1.yaml
+/usr/local/bin/kubectl create -f ~/.apps_config.yaml
